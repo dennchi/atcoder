e9fe820117a43e92e8a0f7a2640e53b85fe5afff
dennchi <bosatsu2211@gmail.com>
2020-12-14 10:27:12 +0900

    OK

--- tools/combination.cpp
+++ tools/combination.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-uint64_t combinations(uint64_t n, uint64_t k) {
+uint64_t comb(uint64_t n, uint64_t k) {
     uint64_t r = 1;
     for (uint64_t d = 1; d <= k; ++d) {
         r *= n--;
