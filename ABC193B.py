95253d7170aad0fde33e3b96eed70b5a8d25708a
dennchi <bosatsu2211@gmail.com>
2021-03-28 23:56:47 +0900

    change

--- ABC193B.py
+++ ABC193B.py
@@ -3,11 +3,11 @@ N = int(input())
 tmp = 2000000000
 
 for i in range(N):
-    A,P,X = list(map(int , inpuut().split()))
+    A,P,X = list(map(int , input().split()))
     if X > A:
         tmp = min(tmp,P)
 
-if tmp = 2000000000:
+if tmp == 2000000:
     print(-1)
 else:
     print(tmp)
